db4fa0a78ee93b81becfef31716ace332ee1b40d
singhDiksha3132 <singhdiksha3132@gmail.com>
2025-07-21 09:50:48 +0000

    data-type

---
+++ 02-basics/01-datatype.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all js code as newer version
+
+// alert (3+3) // we are using node.js , not browser 
+
+console.log(3
+    +3
+) // code readebility should be high
+
+let name = "diksha"
+let age = 18
+let isloggedIn = false
+
+
+// number => 2 to power 53
+// bigint => ye bde numbers ke liye use hota hai jase trading and for compeny like facebook 
+// String => isko hum single code me bhi use kar sakte hai or double code me bhi single code ('') double code ("")
+// boolean => true/false 
+// null => standalone value jisme koi bhi valu nhi ho use null khte hai even 0 bhi na aye null ka type ( object) hota hai 
+// undefined => iska type undefined hi hota hai 
+// symbol => unique type hota hai 
+
+
+
+console.log(typeof age);
