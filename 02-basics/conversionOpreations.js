e8b2a788a24cfbb69ef272253b55b1b5f4e25e8d
singhDiksha3132 <singhdiksha3132@gmail.com>
2025-07-23 07:03:21 +0000

    conversion or data file

--- 02-basics/conversionOpreations.js
+++ 02-basics/conversionOpreations.js
@@ -1,1 +1,33 @@
-console.log("6 + 4 ");
+      // topic is conversion
+let score = "hitesh"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);                        // NAN ka matb hota hai not a number//
+
+//"33" => 33
+// "33abcd" => NaN
+// true => 1;    false => 0
+// these are basic conversion                // true is a boolean valu iske baare me or janna hai //
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+
+// agr 1 ko boolean me convert kre ge to humko true mile ga //0 kre ge false mile ga //
+// 1 => true; 0 => false 
+// "" => false 
+// "diksha" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
