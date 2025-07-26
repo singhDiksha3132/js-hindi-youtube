58230287b864b2603c6424fdd6ef85a2cce860bd
singhDiksha3132 <singhdiksha3132@gmail.com>
2025-07-26 09:26:19 +0000

    data

--- 02-basics/05-datatypessum.js
+++ 02-basics/05-datatypessum.js
@@ -1,2 +1,17 @@
+// ****************************some notes *************************************
+// data types two types ke hote hai first primitive  second non primitive //
+//diffrance beetween 
+//kis trh se data ko memory me raha jata hai ya accses kiya jata hai us bases pe data ka 2 trike se hai primitive or non primitive..
+
 //  primitive 
-console.string(2 + 2);
+// primitive types of data besics it is 7 categories primitive call by valu hote hai 
+//   string, number,boolearn , null , undefined , symbole ,bigInt
+
+const score = 100
+const scoreValue = 100.3  
+
+const isLoggedIn = false
+
+console.table ( score , scoreValue , isLoggedIn );
+// reference type data (isko hum *non primitive* data bhi khate hai )
+//Array , objects ,functions
