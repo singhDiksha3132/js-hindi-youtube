a53f596a8bd4781510760d38117d6562fe41d0a8
singhDiksha3132 <singhdiksha3132@gmail.com>
2025-07-26 09:26:33 +0000

    data

---
+++ 02-basics/02-variable.js
@@ -0,0 +1,13 @@
+const accountId =1414141
+let accountEmail = "diksha123@gmail.com"
+var accountPassword = "123456789"
+accountCity = "jaipur"
+
+
+// accountId = 2 // not allowed
+
+console.log ("accountId");
+accountEmail = "dik12@d.com"
+accountPassword = "121512"
+accountCity = "city"
+console.table([ accountId, accountEmail, accountPassword, accountCity])
